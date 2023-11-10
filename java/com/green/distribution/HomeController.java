63137f2b7b9b288a759aa3a86f71fd156a5248ae
Peh9397 <msch7356@naver.com>
2023-11-10 15:31:06 +0900

    끝

--- java/com/green/distribution/HomeController.java
+++ java/com/green/distribution/HomeController.java
@@ -12,39 +12,35 @@ import com.green.distribution.model.Employee;
 import com.green.distribution.service.EmployeeService;
 
 @Controller
-public class HomeController {
+public class HomeController {	
 	@Autowired
 	private EmployeeService es;
-
+	
 	@GetMapping("loginForm")
 	public String loginForm() {
 		return "/nolay/loginForm";
 	}
-
+	
 	@PostMapping("login")
 	public void login(Employee employee, Model model, HttpSession session) {
 		int result = 0;
-		Employee employee2 = es.select(employee.getEMPCD());
-		if (employee2 == null || employee2.getDel().equals("y"))
-			result = -1; // 없는cd
-		else if (employee2.getPASSWORD().equals(employee.getPASSWORD())) {
-			result = 1; // 성공(일치)
-			session.setAttribute("EMPCD", employee2.getEMPCD());
-			session.setAttribute("ENAME", employee2.getENAME());
-			session.setAttribute("JOB", employee2.getJOB());
-		}
+		Employee employee2 = es.select(employee.getEmployeeCd());
+		if (employee2 == null || employee2.getDel().equals("y")) result = -1;	// 없는cd
+		else if (employee.getPassword().equals(employee2.getPassword())) {
+			result = 1;	// 성공(일치)
+			session.setAttribute("employeeCd", employee.getEmployeeCd());
+		}	
 		model.addAttribute("result", result);
 	}
-
+	
 	@GetMapping("logout")
 	public String logout(HttpSession session) {
-		session.invalidate(); // invalidate : session 데이터 지우기, logout은 세션을 날리는 것
+		session.invalidate();	// invalidate : session 데이터 지우기, logout은 세션을 날리는 것
 		return "/logout";
 	}
-
+	
 	@GetMapping("main")
 	public String main() {
 		return "/main";
 	}
-
 }
